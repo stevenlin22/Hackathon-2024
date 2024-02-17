9a9481917ae3eb804680c81900b96149d8d2d0ba
mtj8 <140464071+mtj8@users.noreply.github.com>
2024-02-17 20:10:41 +0000

    move init to controller

--- project/src/controller.py
+++ project/src/controller.py
@@ -2,7 +2,25 @@
 # In this case the models are the structure of the game (sprites, buttons, etc.), the view is pygame, and the controller is this file which initializes and works with the two.
 
 # import necessary files and modules
+import pygame
 
 class Controller:
     def __init__(self):
+        pygame.init()
+
+        screen = pygame.display.set_mode([300, 400])
+        pygame.display.set_caption("HackBU clicker game")
+
+        running = True
+        while running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+
+
+        pygame.quit()
+    
+    def example(self):
         pass
