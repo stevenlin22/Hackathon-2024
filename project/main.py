5b755f5739d59543ebba12ba6280f848d4de9581
s n <sni1@binghamton.edu>
2024-02-17 15:00:08 -0500

    main game loop + screen

--- project/main.py
+++ project/main.py
@@ -1,1 +1,17 @@
 # main
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode([300, 400])
+pygame.display.set_caption("HackBU clicker game")
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
+pygame.quit()
