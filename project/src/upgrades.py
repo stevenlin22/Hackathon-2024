2b40b48101960291d8e5fd27dbfc406d50552f83
mtj8 <140464071+mtj8@users.noreply.github.com>
2024-02-17 20:03:57 +0000

    upgrade suggestion

--- project/src/upgrades.py
+++ project/src/upgrades.py
@@ -2,8 +2,10 @@ class Upgrades:
     def __init__(self):
         """initializes upgrades where multiplier is the multiplier and the increase rate is the rate at which the score increases
         """
+        # Matt: shouldn't we take in multiplier and increaserate as args so we can change it on a case-by-case basis?
         self.multiplier = 1
         self.increaseRate = 1
+
     def scoreIncrease(self, score):
         """Run inside the event loop on click. Increases the score based on multiplier
 
