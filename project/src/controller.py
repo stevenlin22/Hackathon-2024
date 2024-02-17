613fb782950e48df8d9b51a86de2f2eb756c6adc
stevenlin22 <slin22@binghamton.edu>
2024-02-17 17:28:07 -0500

    Click

--- project/src/controller.py
+++ project/src/controller.py
@@ -3,46 +3,48 @@
 
 # import necessary files and modules
 import pygame
+from src.upgrades import Upgrades
 
 class Controller:
     # init
     def __init__(self):
         pygame.init()
 
-        screen = pygame.display.set_mode([300, 400])
+        self.screen = pygame.display.set_mode([300, 400])
+        self.screen.fill("white")
+        self.width, self.length = pygame.display.get_window_size()
         pygame.display.set_caption("HackBU clicker game")
-        framerate = 60
-        timer = pygame.time.Clock()
-
+        self.framerate = 60
+        self.timer = pygame.time.Clock()
+        multiplier = 1
+        increaseRate = 1
+        self.score_increase = Upgrades(multiplier, increaseRate)
         self.state = "MAIN"
-
 
     # the gameloop manages game state
     def gameloop(self):
-        
         if self.state == "MAIN":
             self.mainloop()
         elif self.state == "PLACEHOLDER":
             self.placeholder()
 
 
-    
-
     # mainloop is the main game (not the start, or end, etc.)
     def mainloop(self):
 
         running = True
+        score = 0
         while running:
-            timer.tick(framerate)
+            # timer.tick(framerate)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        score = self.score_increase.scoreIncrease(score)
+                        print(score)
             pygame.display.flip()
-
         pygame.quit()
-
-    
 
     def placeholder(self):
         pass
